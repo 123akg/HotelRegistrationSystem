16329fe47e0f418bbecf88888c8564d064f96b36
123akg <abhijeetkumargiri7070@gmail.com>
2020-10-11 23:49:11 +0530

    Add: Welcome to Hotel Registration System

---
+++ src/com/capgemini/hotelregistrationsystem/HotelRegistration.java
@@ -0,0 +1,10 @@
+package com.capgemini.hotelregistrationsystem;
+
+public class HotelRegistration {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation Program");
+
+	}
+
+}
